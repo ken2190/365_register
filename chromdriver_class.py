6c2d5f44fb137596458d555b7bac20ff0733cf11
Sergey <pristup2040@gmail.com>
2022-04-19 21:34:43 +1000

    add main2.py and chrome browser

--- chromdriver_class.py
+++ chromdriver_class.py
@@ -137,129 +137,87 @@ class FireFoxDriverWithVPN():
         self.driver.switch_to.default_content()
 
 
-class GetWorkAccounts:
-    def __init__(self, number_of_accounts, path_to_firefox_profile):
-        self.firefox_profile = path_to_firefox_profile
-        self.number_of_accounts = number_of_accounts
+class ChromeDriver:
+    def __init__(self,
+                 user_data_dir=data.path_to_chrome_user_dir,
+                 profile_directory=data.chrome_profile_name,
+                 ):
+        self.user_data_dir = user_data_dir
+        self.profile_directory = profile_directory
 
-        def check_bet365(driver):
-            # провепка правильно ли открылся сайт bet365
-            try:
-                time.sleep(2)
-                driver.find_element_by_class_name('hm-MainHeaderRHSLoggedOutWide_LoginContainer')
-                return True
-            except Exception as er:
-                return False
+        options = webdriver.ChromeOptions()
+        options.add_experimental_option("excludeSwitches", ["enable-automation"])
+        options.add_experimental_option('useAutomationExtension', False)
+        options.add_argument(f'user-data-dir={self.user_data_dir}')
+        options.add_argument(f"profile-directory={self.profile_directory}")
+        # options.add_argument(f"--incognito")
+        self.driver = webdriver.Chrome(options=options)
 
-        def open_new_window_2ip(driver):
-            current_window = driver.current_window_handle
-            driver.execute_script(f"window.open('https://2ip.ru/', '_blank')")
-            time.sleep(7)
-            driver.switch_to.window(driver.window_handles[-1])
-            driver.close()
-            driver.switch_to.window(current_window)
+    def start_registration(self):
+        time.sleep(4)
+        self.driver.find_element_by_class_name('hm-MainHeaderRHSLoggedOutWide_Join').click()
+        time.sleep(10)
+        self.driver.switch_to.frame(self.driver.find_element_by_class_name('mim-MembersIframeModule_Iframe '))
+        time.sleep(1)
+        self.driver.switch_to.frame(self.driver.find_element_by_class_name('lm-LegacyModule_IFrame '))
+        time.sleep(1)
 
-        def get_driver():
-            firefox_capabilities = webdriver.DesiredCapabilities.FIREFOX
-            firefox_capabilities['marionette'] = True
+    def human_input(self, input_text):
+        '''Вводит текс, предварительно нужно нажать на форму'''
 
-            fp = webdriver.FirefoxProfile(self.firefox_profile)
-            fp.set_preference("browser.privatebrowsing.autostart", True)
+        time.sleep(0.3)
+        for simvol in str(input_text):
+            self.driver.find_element_by_tag_name("body").send_keys(simvol)
+            time.sleep(random.randint(15, 40) / 100)
+        time.sleep(random.randint(1, 4))
 
-            options = webdriver.FirefoxOptions()
-            options.add_argument("-private")
-            options.set_preference("dom.webdriver.enabled", False)
-            options.set_preference("dom.webnotifications.enabled", False)
-            binary = data.firefox_binary
-            options.binary = binary
+    def select_element(self, selection_obj, el_value='', el_visible_text=''):
+        from selenium.webdriver.support.ui import Select
 
-            driver = webdriver.Firefox(capabilities=firefox_capabilities, firefox_profile=fp,
-                                       firefox_binary=data.firefox_binary,
-                                       executable_path=data.path_to_geckodriver,
-                                       options=options)
+        select = Select(selection_obj)
 
-            time.sleep(10)
+        if el_visible_text != '':
+            select.select_by_visible_text(el_visible_text)
+        else:
+            select.select_by_value(el_value)
+        time.sleep(random.randint(1, 4))
 
-            driver.get('https://2ip.ru/')
-            driver.set_page_load_timeout(15)
-            try:
-                driver.get('https://www.bet365.com/')
-                driver.set_page_load_timeout(25)
-                if check_bet365(driver):
-                    return driver, 'OK'
-            except:
-                pass
+    def select_in_selection(self):
+        from selenium.webdriver.support.ui import Select
+        select = Select(self.driver.find_element_by_id('Title'))
+        select.select_by_visible_text('Mrs')
 
-            driver.set_page_load_timeout(25)
-            for i in range(2):
-                open_new_window_2ip(driver)
-                time.sleep(0.3)
+        print(1)
+        time.sleep(5)
+        select = Select(self.driver.find_element_by_id('DateOfBirthDay'))
+        select.select_by_value('1')
+        print(1)
 
-            try:
-                driver.get('https://www.bet365.com/')
-                if check_bet365(driver):
-                    return driver, 'OK'
-                else:
-                    return driver, 'Сайт bet365 не загрузился'
-            except:
-                return driver, 'Сайт bet365 не загрузился'
+        time.sleep(5)
+        select = Select(self.driver.find_element_by_id('DateOfBirthMonth'))
+        select.select_by_value('2')
+        print(1)
 
-        def add_accounts_to_list(Browsers_List=[]):
-            # задержка
-            time_to_sleep = random.randint(1, 1000) / 500
-            time.sleep(time_to_sleep)
-            driver, info = get_driver()
-            if info == 'OK':
-                Browsers_List.append(driver)
-                print('+1 browser')
-            else:
-                try:
-                    driver.close()
-                    driver.quit()
-                except:
-                    pass
+        time.sleep(5)
+        select = Select(self.driver.find_element_by_id('DateOfBirthMonth'))
+        select.select_by_value('1975')
+        # select.select_by_index(index)
+        # select.select_by_visible_text("text")
+        # select.select_by_value(value)
 
-        # число браузеров, которое будет открыто
-        number_of_tries = 6
-        try:
-            number_of_tries = data.number_of_tries
-        except:
-            pass
-        Browser_List = []
+    def open_new_window_2ip(self):
+        current_window = self.driver.current_window_handle
+        print('open site 2ip.ru')
+        self.driver.execute_script(f"window.open('https://2ip.ru/', '_blank')")
+        time.sleep(5)
+        self.driver.switch_to.window(self.driver.window_handles[-1])
+        self.driver.close()
+        self.driver.switch_to.window(current_window)
 
-        while len(Browser_List) < self.number_of_accounts:
-            try:
-                with Pool(processes=number_of_tries) as p:
-                    p.map(add_accounts_to_list, [Browser_List for i in range(number_of_tries)])
-            except Exception as er:
-                print(f'Ошибка при выполнениии Poll: {er}')
-
-            # check_counter_i = 0
-            # while check_counter_i < len(Browser_List):
-            #     if check_bet365(Browser_List[check_counter_i]):
-            #         check_counter_i += 1
-            #     else:
-            #         print('Браузер не работает!')
-            #         Browser_List.pop(check_counter_i)
-
-            print(f'Открыто {len(Browser_List)} из {self.number_of_accounts} аккаунтов')
-
-
-        while len(Browser_List) > self.number_of_accounts:
-            Browser_List.pop(-1).quit()
-            print('1 лишний аккаунт удалён')
-
-        self.Browser_List = Browser_List
-
-    def return_Browser_List(self):
-        return self.Browser_List
-
-
-
-class FireFoxDriverWithVPN_multipotok_open(FireFoxDriverWithVPN):
-    def __init__(self, path_to_firefox_profile):
-        self.is_VPN = True
-        get_accounts_class = GetWorkAccounts(number_of_accounts=1, path_to_firefox_profile=path_to_firefox_profile)
-        self.driver = get_accounts_class.return_Browser_List()[0]
-
+    def close_last_window(self):
+        current_window = self.driver.current_window_handle
+        self.driver.switch_to.window(self.driver.window_handles[-1])
+        self.driver.close()
+        # self.driver.switch_to.window(current_window)
+        self.driver.switch_to.default_content()
 
---
+++ main2.py
@@ -0,0 +1,260 @@
+import random
+import time
+import datetime
+
+from selenium.webdriver import ActionChains
+
+import data
+from chromdriver_class import ChromeDriver
+from get_users_data_from_google_sheets import sheet_user_values
+from random_for_registration import RandomDateRegister
+from google_sheets_writer import GoogleWriter1
+
+
+CURENT_NUMBER = 1000
+
+try:
+    GoogleWriter1.write_row(["Дата", "№", "Логин", "Пароль", "Пин", "Строка", "e-mail", "Статус"])
+except Exception as er:
+    print(er)
+    print('Ошибка, откройте доступ к странице')
+    exit()
+
+
+# c какой позиции читаем данные   1 -> вторая запись в таблице поле заголовков :)
+start_line = 1
+# c какой позиции записываем данные  2 -> вторая запись в таблице поле заголовков
+GoogleWriter1.current_row = 2
+
+
+while True:
+    if start_line >= len(sheet_user_values):
+        print('Все аккаунты зарегистрированы!')
+        break
+
+    user_data = sheet_user_values[start_line]
+    print(f'Обработка аккаунта {start_line}/{len(sheet_user_values)-1}', '-'*100)
+
+    driver1 = ChromeDriver()
+
+    try:
+        driver1.start_registration()
+    except:
+        print('Не удолось начать регистрировать аккаунт')
+        driver1.driver.close()
+        driver1.driver.quit()
+        continue
+    try:
+        driver1.driver.find_element_by_xpath("//span[text()='Username taken.']")
+    except:
+        pass
+
+    # поле обращение Mr/Mrs
+    if user_data[0] == 'Ж':
+        select_obj = driver1.driver.find_element_by_id('Title')
+        driver1.select_element(select_obj, el_visible_text='Mrs')
+
+    # ввод 1 имени
+    driver1.driver.find_element_by_id('FirstName').click()
+    driver1.human_input(user_data[1])
+
+    # ввод 2 имени
+    driver1.driver.find_element_by_id('Surname').click()
+    driver1.human_input(user_data[2])
+
+    # ввод даты рождения
+    day, month, year = user_data[3].split('.')
+    day = str(int(day))
+    month = str(int(month))
+
+    select_obj_day = driver1.driver.find_element_by_id('DateOfBirthDay')
+    driver1.select_element(select_obj_day, el_value=day)
+
+    select_obj_month = driver1.driver.find_element_by_id('DateOfBirthMonth')
+    driver1.select_element(select_obj_month, el_value=str(int(month)-1))
+
+    select_obj_year = driver1.driver.find_element_by_id('DateOfBirthYear')
+    driver1.select_element(select_obj_year, el_value=year)
+
+    # генерация случайных данных
+    r = RandomDateRegister(user_data[1], user_data[2])
+    email_ = r.get_email()
+    phone_ = r.get_phone()
+    password_ = r.get_password()
+    pincode_ = r.get_pincode()
+
+    # заполнение email
+    driver1.driver.find_element_by_id('EmailAddress').send_keys('')
+    time.sleep(random.randint(10, 20)/10)
+    driver1.human_input(email_)
+    print(f'email: {email_}')
+
+    # заполнение номера
+    driver1.driver.find_element_by_id('PhoneNumber').send_keys('')
+    time.sleep(random.randint(10, 20)/10)
+    driver1.human_input(phone_)
+    # для скрола
+    driver1.driver.find_element_by_id('Password').send_keys('')
+    time.sleep(random.randint(2, 5))
+
+    # заполнение адреса
+    driver1.driver.find_element_by_id('CurrentBuildingNumberSearch').send_keys('')
+    adress_text = user_data[4].split(',')[0]
+    # print(adress_text)
+    driver1.human_input(adress_text)
+    time.sleep(2)
+    # index адреса
+    driver1.driver.find_element_by_id('CurrentPostcodeSearch').click()
+    driver1.human_input(user_data[6])
+    # нажимаем на кнопку поиска адреса
+    driver1.driver.find_element_by_id('CurrentFindAddress').click()
+    time.sleep(random.randint(5, 7))
+    try:
+        driver1.driver.find_element_by_id('CurrentAddress_0').click()
+        time.sleep(2)
+    except:
+        pass
+    # новое (17.12.2021) ввод города
+    time.sleep(1)
+    try:
+        driver1.driver.find_element_by_id('CurrentTownCity').click()
+        time.sleep(random.randint(2, 3))
+    except:
+        pass
+    driver1.human_input(user_data[5])
+
+    time.sleep(random.randint(1, 3))
+
+    # для скрола
+    el1 = driver1.driver.find_element_by_id('Password')
+    ActionChains(driver1.driver).move_to_element(el1).click(el1).perform()
+    driver1.driver.find_element_by_id('Password').send_keys('')
+    time.sleep(random.randint(2, 3))
+
+    # login + password
+    driver1.driver.find_element_by_id('UserName').click()
+    login_ = r.get_login()
+    driver1.human_input(login_)
+    driver1.driver.find_element_by_id('Password').click()
+    time.sleep(2)
+    # ввод логина
+    while True:
+        try:
+            # ввод нового логина
+            driver1.driver.find_element_by_xpath("//span[text()='Username taken.']")
+            driver1.driver.find_element_by_id('UserName').clear()
+            driver1.driver.find_element_by_id('UserName').send_keys('')
+            login_ = r.get_more_random_login()
+            driver1.human_input(login_)
+            time.sleep(1)
+            driver1.driver.find_element_by_id('Password').click()
+            time.sleep(2)
+        except:
+            break
+    print(f'login: {login_}')
+
+    time.sleep(random.randint(40, 50)/10)
+    # ввод пароля
+    driver1.driver.find_element_by_id('Password').click()
+    driver1.human_input(password_)
+    time.sleep(random.randint(40, 50)/10)
+    print(f'password: {password_}')
+
+
+    # проматываем вниз
+    try:
+        driver1.driver.find_element_by_id('Submit').send_keys('')
+    except Exception as er:
+        print(er)
+
+    # ввод и повторение пина
+    driver1.driver.find_element_by_id('FourDigitPin').click()
+    time.sleep(random.randint(10, 30)/10)
+    driver1.human_input(pincode_)
+    time.sleep(random.randint(5, 20)/10)
+
+    driver1.driver.find_element_by_id('FourDigitPinConfirmed').click()
+    time.sleep(random.randint(10, 30)/10)
+    driver1.human_input(pincode_)
+    time.sleep(random.randint(20, 50)/10)
+
+    print(f'pin: {pincode_}')
+
+    # NoThanksRadio
+    driver1.driver.find_element_by_id('NoThanksRadio').click()
+    time.sleep(random.randint(20, 30)/10)
+
+    # мне больше 18
+    driver1.driver.find_element_by_id('PoliciesAcceptance').click()
+    time.sleep(random.randint(15, 25)/10)
+
+    # конец регистрации !
+    driver1.driver.find_element_by_id('Submit').click()
+    time.sleep(45)
+
+    is_good = False
+    driver1.driver.switch_to.default_content()
+
+    try:
+        frame = driver1.driver.find_element_by_id('MembersIframe')
+        driver1.driver.switch_to.frame(frame)
+        driver1.driver.find_element_by_class_name('nh-NavigationHeaderModule_Title ')
+        no_valid_flag = False
+
+        # try:
+        #     frame2 = driver1.driver.find_element_by_id('MembersHostFrame')
+        #     driver1.driver.switch_to.frame(frame2)
+        #     driver1.driver.find_element_by_class_name('pm-debitcard')
+        #     print('порезан1')
+        #     no_valid_flag = True
+        # except:
+        #     pass
+        try:
+            frame2 = driver1.driver.find_element_by_id('MembersHostFrame')
+            driver1.driver.switch_to.frame(frame2)
+            driver1.driver.find_element_by_class_name('withdrawal-restriction')
+            print('порезан2')
+            no_valid_flag = True
+        except:
+            pass
+
+        if no_valid_flag:
+            raise Exception('Не рабочий аккунт')
+        print('Рабочий аккаунт')
+        is_good = True
+    except:
+        print('Порезан')
+
+    if is_good:
+        status_ = 'Готов к работе'
+    else:
+        status_ = 'Порезан'
+
+
+    driver1.driver.close()
+    driver1.driver.quit()
+    start_line += 1
+
+    # время
+    current_date = datetime.datetime.now()
+    current_date_string = current_date.strftime('%m.%d.%Y')
+
+    data_ = [
+        str(current_date_string), # time now
+        CURENT_NUMBER + start_line, # number
+        login_, # login
+        password_, # password
+        pincode_, # pin
+        ' '.join(user_data), # трока с информацией о пользователе
+        email_, # email
+        status_
+    ]
+    print(data_)
+    GoogleWriter1.write_row(data_)
+
+    print(f'Аккаунт {start_line-1}/{len(sheet_user_values)-1} успешно обработан', '-'*100)
+
+
+print('End.')
+# driver1.select_in_selection()
+

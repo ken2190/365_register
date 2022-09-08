7cb3e3e3f173427f01f748578344fee058fe43c5
Sergey <pristup2040@gmail.com>
2022-09-08 21:00:49 +1000

    .\main.py

--- main.py
+++ main.py
@@ -5,11 +5,10 @@ import datetime
 from selenium.webdriver import ActionChains
 
 import data
-from chromdriver_class import ChromeDriver, ChromeDriverProxy
+from chromdriver_class import ChromeDriverProxy
 from get_users_data_from_google_sheets import sheet_user_values, number_of_list2_lines
 from random_for_registration import RandomDateRegister
 from google_sheets_writer import GoogleWriter1
-# from profile_dir_worker import ProfileDirWorker1
 
 CURENT_NUMBER = 1000
 
@@ -37,14 +36,7 @@ while True:
     user_data = sheet_user_values[start_line]
     print(f'Обработка аккаунта {start_line}/{len(sheet_user_values)-1}', '-'*100)
 
-    # try:
-    #     ProfileDirWorker1.del_copy()
-    # except Exception as er:
-    #     print(f'dir: {er}')
-    # print('Создаём копию профиля---------------')
-    # ProfileDirWorker1.create_copy()
 
-    # driver1 = ChromeDriver()
     driver1 = ChromeDriverProxy()
 
     driver1.driver.get('https://www.bet365.com/#/HO/')
@@ -53,7 +45,12 @@ while True:
         # Cookies
         driver1.driver.find_element_by_class_name('ccm-CookieConsentPopup_Accept ').click()
     except:
-        pass
+        time.sleep(10)
+        try:
+            # Cookies
+            driver1.driver.find_element_by_class_name('ccm-CookieConsentPopup_Accept ').click()
+        except:
+            pass
     # input('k')
     try:
         driver1.start_registration()
@@ -62,25 +59,26 @@ while True:
         driver1.driver.close()
         driver1.driver.quit()
         continue
-    try:
-        driver1.driver.find_element_by_xpath("//span[text()='Username taken.']")
-    except:
-        pass
 
     # выбор страны UK
-    select_obj = driver1.driver.find_element_by_id('Country')
+    select_obj = driver1.driver.find_element_by_id('country_residence')
     driver1.select_element(select_obj, el_visible_text='United Kingdom')
     time.sleep(3)
-    driver1.driver.find_element_by_id('Confirm').click()
-    time.sleep(10)
+    # driver1.driver.find_element_by_id('Confirm').click()
+    try:
+        driver1.driver.find_element_by_class_name('oam-ConfirmButton ').click()
+    except Exception as er:
+        print(f'Не удалось изменит страну: {er}')
+    time.sleep(3)
 
     # поле обращение Mr/Mrs
-    if user_data[0] == 'F':
-        select_obj = driver1.driver.find_element_by_id('Title')
-        driver1.select_element(select_obj, el_visible_text='Mrs')
+    if user_data[0] == 'MISS':
+        select_obj = driver1.driver.find_element_by_id('title')
+        driver1.select_element(select_obj, el_visible_text='Miss')
 
+    time.sleep(4)
     # ввод 1 имени
-    element_fisrstName = driver1.driver.find_element_by_id('FirstName')
+    element_fisrstName = driver1.driver.find_element_by_id('first_name')
     driver1.human_input2_new(user_data[1], element_fisrstName)
     # driver1.driver.find_element_by_id('FirstName').send_keys('123')
     # driver1.driver.find_element_by_id('FirstName').click()
@@ -90,7 +88,7 @@ while True:
     # input('123')
 
     # ввод 2 имени
-    elemen_Surname = driver1.driver.find_element_by_id('Surname')
+    elemen_Surname = driver1.driver.find_element_by_id('last_name')
     driver1.human_input2_new(user_data[2], elemen_Surname)
     # driver1.driver.find_element_by_id('Surname').click()
     # driver1.human_input(user_data[2])
@@ -100,13 +98,17 @@ while True:
     day = str(int(day))
     month = str(int(month))
 
-    select_obj_day = driver1.driver.find_element_by_id('DateOfBirthDay')
+    select_obj_day = driver1.driver.find_elements_by_class_name('oam-DateSelectComponent-1 ')[0]
+    if len(day) == 1:
+        day = '0' + day
     driver1.select_element(select_obj_day, el_value=day)
 
-    select_obj_month = driver1.driver.find_element_by_id('DateOfBirthMonth')
-    driver1.select_element(select_obj_month, el_value=str(int(month)-1))
+    select_obj_month = driver1.driver.find_elements_by_class_name('oam-DateSelectComponent-1 ')[1]
+    if len(month) == 1:
+        month = '0' + month
+    driver1.select_element(select_obj_month, el_value=month)
 
-    select_obj_year = driver1.driver.find_element_by_id('DateOfBirthYear')
+    select_obj_year = driver1.driver.find_elements_by_class_name('oam-DateSelectComponent-1 ')[2]
     driver1.select_element(select_obj_year, el_value=year)
 
     # генерация случайных данных
@@ -117,27 +119,32 @@ while True:
     pincode_ = r.get_pincode()
 
     # заполнение email
-    driver1.driver.find_element_by_id('EmailAddress').send_keys('')
+    driver1.driver.find_element_by_id('email').send_keys('')
     time.sleep(random.randint(10, 20)/10)
-    element_email = driver1.driver.find_element_by_id('EmailAddress')
+    element_email = driver1.driver.find_element_by_id('email')
     driver1.human_input2_new(email_, element_email)
     # driver1.human_input(email_)
     print(f'email: {email_}')
 
     # заполнение номера
-    driver1.driver.find_element_by_id('PhoneNumber').send_keys('')
+    driver1.driver.find_element_by_id('tel').send_keys('')
     time.sleep(random.randint(10, 20)/10)
-    element_phone = driver1.driver.find_element_by_id('PhoneNumber')
+    element_phone = driver1.driver.find_element_by_id('tel')
     driver1.human_input2_new(phone_, element_phone)
-    # driver1.human_input(phone_)
+    print(f'phone: {phone_}')
+
+
+    # не соглащаемся с уведомлениями
+    driver1.driver.find_elements_by_class_name('oam-OARadioButton_Radio ')[1].click()
+    time.sleep(3)
     # для скрола
-    driver1.driver.find_element_by_id('Password').send_keys('')
+    driver1.driver.find_element_by_id('password').send_keys('')
     time.sleep(random.randint(2, 5))
 
     # заполнение адреса
     adress_text = user_data[4]
-    element_CurrentBuildingNumberSearch = driver1.driver.find_element_by_id('CurrentBuildingNumberSearch')
-    driver1.driver.find_element_by_id('CurrentBuildingNumberSearch').send_keys('')
+    element_CurrentBuildingNumberSearch = driver1.driver.find_element_by_id('addr_bld_no')
+    driver1.driver.find_element_by_id('addr_bld_no').send_keys('')
     driver1.human_input2_new(adress_text, element_CurrentBuildingNumberSearch)
     # driver1.human_input(adress_text)
     time.sleep(2)
@@ -152,22 +159,22 @@ while True:
 
     # index адреса
     # input('Вводим postcode:')
-    driver1.driver.find_element_by_id('CurrentPostcodeSearch').click()
-    element_CurrentPostcodeSearch = driver1.driver.find_element_by_id('CurrentPostcodeSearch')
+    driver1.driver.find_element_by_id('addr_pst_zip').click()
+    element_CurrentPostcodeSearch = driver1.driver.find_element_by_id('addr_pst_zip')
     driver1.human_input2_new(user_data[6], element_CurrentPostcodeSearch)
     # driver1.human_input(user_data[6])
     # нажимаем на кнопку поиска адреса
-    driver1.driver.find_element_by_id('CurrentFindAddress').click()
+    driver1.driver.find_element_by_id('find_address').click()
     time.sleep(random.randint(10, 12))
     try:
-        driver1.driver.find_element_by_id('CurrentAddress_0').click()
-        time.sleep(2)
+        driver1.driver.find_elements_by_class_name('oam-AddressLabel ')[1].click()
+        time.sleep(4)
     except:
         pass
 
     # новое (17.12.2021) ввод города
     try:
-        element_CurrentTownCity = driver1.driver.find_element_by_id('CurrentTownCity')
+        element_CurrentTownCity = driver1.driver.find_element_by_id('addr_twn_cty')
         print(element_CurrentTownCity.get_attribute("value"))
         if element_CurrentTownCity.get_attribute("value") == '':
             driver1.human_input2_new(user_data[5], element_CurrentTownCity)
@@ -177,31 +184,31 @@ while True:
     time.sleep(random.randint(1, 3))
 
     # для скрола
-    el1 = driver1.driver.find_element_by_id('Password')
+    el1 = driver1.driver.find_element_by_id('password')
     # ActionChains(driver1.driver).move_to_element(el1).click(el1).perform()
-    driver1.driver.find_element_by_id('Password').send_keys('')
+    driver1.driver.find_element_by_id('password').send_keys('')
     time.sleep(random.randint(2, 3))
 
     # login + password
-    driver1.driver.find_element_by_id('UserName').click()
+    driver1.driver.find_element_by_id('username').click()
     login_ = r.get_login()
     # driver1.human_input(login_)
-    element_login = driver1.driver.find_element_by_id('UserName')
+    element_login = driver1.driver.find_element_by_id('username')
     driver1.human_input2_new(login_, element_login)
-    driver1.driver.find_element_by_id('Password').click()
+    driver1.driver.find_element_by_id('password').click()
     time.sleep(2)
     # ввод логина
     while True:
         try:
             # ввод нового логина
-            driver1.driver.find_element_by_xpath("//span[text()='Username taken.']")
-            driver1.driver.find_element_by_id('UserName').clear()
-            driver1.driver.find_element_by_id('UserName').send_keys('')
+            driver1.driver.find_element_by_xpath("//div[text()='Username taken.']")
+            driver1.driver.find_element_by_id('username').clear()
+            driver1.driver.find_element_by_id('username').send_keys('')
             login_ = r.get_more_random_login()
             # driver1.human_input(login_)
             driver1.human_input2_new(login_, element_login)
             time.sleep(1)
-            driver1.driver.find_element_by_id('Password').click()
+            driver1.driver.find_element_by_id('password').click()
             time.sleep(2)
         except:
             break
@@ -209,8 +216,8 @@ while True:
 
     time.sleep(random.randint(40, 50)/10)
     # ввод пароля
-    driver1.driver.find_element_by_id('Password').click()
-    element_password = driver1.driver.find_element_by_id('Password')
+    driver1.driver.find_element_by_id('password').click()
+    element_password = driver1.driver.find_element_by_id('password')
     driver1.human_input2_new(password_, element_password)
     # driver1.human_input(password_)
     time.sleep(random.randint(40, 50)/10)
@@ -223,37 +230,32 @@ while True:
     except Exception as er:
         print(er)
 
-    # ввод и повторение пина
-    driver1.driver.find_element_by_id('FourDigitPin').click()
-    time.sleep(random.randint(10, 30)/10)
-    element_pincode = driver1.driver.find_element_by_id('FourDigitPin')
-    driver1.human_input2_new(pincode_, element_pincode)
-    # driver1.human_input(pincode_)
-    time.sleep(random.randint(5, 20)/10)
-
-    driver1.driver.find_element_by_id('FourDigitPinConfirmed').click()
-    time.sleep(random.randint(10, 30)/10)
-    # driver1.human_input(pincode_)
-    element_pincode2 = driver1.driver.find_element_by_id('FourDigitPinConfirmed')
-    driver1.human_input2_new(pincode_, element_pincode2)
-    time.sleep(random.randint(20, 50)/10)
-
-    print(f'pin: {pincode_}')
-
-    # NoThanksRadio
-    driver1.driver.find_element_by_id('NoThanksRadio').click()
-    time.sleep(random.randint(20, 30)/10)
 
     # мне больше 18
-    driver1.driver.find_element_by_id('PoliciesAcceptance').click()
+    driver1.driver.find_element_by_class_name('oam-FieldInputCheckboxTerms_Checkbox ').click()
     time.sleep(random.randint(15, 25)/10)
 
     # конец регистрации !
-    driver1.driver.find_element_by_id('Submit').click()
+    driver1.driver.find_element_by_class_name('oam-FieldSubmitButtonWithModal').click()
     time.sleep(45)
-    # input('check:')
+    input('check:')
+
     is_good = False
     driver1.driver.switch_to.default_content()
+
+    # новая проверка на успешность <-
+    is_good_account = True
+    try:
+        frame = driver1.driver.find_element_by_id('MembersIframe')
+        driver1.driver.switch_to.frame(frame)
+        frame2 = driver1.driver.find_element_by_id('MembersHostFrame')
+        driver1.driver.switch_to.frame(frame2)
+        driver1.driver.find_element_by_class_name('accept-button')
+        print('Порезан')
+        is_good_account = False
+    except:
+        pass
+    # новая проверка на успешность ->
 
     try:
         frame = driver1.driver.find_element_by_id('MembersIframe')
@@ -305,8 +307,6 @@ while True:
     driver1.driver.quit()
     start_line += 1
 
-    print('Удаляем копию профиля---------------')
-    # ProfileDirWorker1.del_copy()
 
     # время
     current_date = datetime.datetime.now()
